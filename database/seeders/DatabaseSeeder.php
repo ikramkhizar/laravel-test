057003208bf139a2ede6cdc5a56223907a77b04b
ikramkhizer <ikramkhizer@gmail.com>
2022-03-25 10:47:43 +0500

    factory added in seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\Product::factory(100)->create();
         // \App\Models\User::factory(10)->create();
     }
 }
